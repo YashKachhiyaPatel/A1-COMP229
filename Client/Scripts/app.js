4b2ab71b2540884c2057d5c2d6a77c974309d610
YashKachhiyaPatel <lifeline242734@gmail.com>
2021-06-04 22:31:00 -0700

    change color

--- Client/Scripts/app.js
+++ Client/Scripts/app.js
@@ -1,18 +1,8 @@
 "use strict";
 (function () {
     function Start() {
-        console.log("App Started");
+        console.log("Welcome, My name is Yash");
     }
     window.addEventListener("load", Start);
 })();
 
-// const currentLocation = location.href;
-// const menuItem = document.querySelectorAll('li');
-// const menuLength = menuItem.length;
-
-// for(let i=0;i<menuLength;i++){
-//     if(menuItem.href === currentLocation){
-//         menuItem[i].className = "active";
-//     }
-// }
-//# sourceMappingURL=app.js.map
